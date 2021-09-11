b7ec26784985e86ebbc851714f3160a4def59e29
dulajkavinda <dulajkaavinda@gmail.com>
2021-09-11 12:27:21 +0530

    models added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './views/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router history={createBrowserHistory()}>
+          <div className="route">
+            <Route exact path="/" component={Home} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
